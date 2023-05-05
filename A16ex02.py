0b060c726139d1e98719cc65063560caed5c2db5
SavioJSilva <129877737+SavioJSilva@users.noreply.github.com>
2023-05-05 11:32:46 -0300

    adicionando exercicio

---
+++ A16ex02.py
@@ -0,0 +1,4 @@
+tabela_brasileirao = ('Botafogo', 'Fortaleza', 'Palmeiras', 'Internacional', 'Fluminense',
+                      'Cruzeiro', 'Grêmio', 'São Paulo', 'Vasco da gama', 'Atlético-MG',
+                      'Santos', 'Bragantino', 'Flamengo', 'Athletico-PR', 'Bahia',
+                      'Goiás', 'Corinthians', 'Cuiabá', 'Coritiba', 'América-MG')
